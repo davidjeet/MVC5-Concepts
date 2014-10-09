68c87dc0a16326c821e24c5d8198d0d279060845
David Green <davidgreen24@gmail.com>
2014-10-09 17:04:04 -0400

    Refactored UI and added Typescript demo

---
+++ Web.UI/Controllers/TestAlertsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PC.RMS.RP.Areas.SelfService.Controllers
+{
+    using Web.UI.Infrastructure.Alerts;
+
+    public class TestAlertsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id = "abc")
+        {
+            //Url.ActionArea()
+            return RedirectToAction("Index").WithWarning(id);
+        }
+    }
+}
---
+++ Web.UI/Controllers/TestTypeScriptController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PC.RMS.RP.Areas.SelfService.Controllers
+{
+    using Web.UI.Infrastructure.Alerts;
+
+    public class TestTypeScriptController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
